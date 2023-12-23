d5480ff9496a69737dafc3a7afed2bc79e51365e
Naveed <naveed.dev30@gmail.com>
2023-12-24 04:23:45 +0500

    Image SVG to canvas

--- lib/src/painter.dart
+++ lib/src/painter.dart
@@ -158,7 +158,7 @@ class _DeepPainter extends CustomPainter {
       canvas.save();
       canvas.clipRect(pictureRect);
       canvas.drawPicture(controller.pictureInfo!.picture);
-      canvas.restore();
+      // canvas.restore();
     }
 
     canvas.saveLayer(Offset.zero & size, Paint());
