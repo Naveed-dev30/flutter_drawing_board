60ac13f15d6a1c4ef4c5edc773569a2f21342481
Naveed <naveed.dev30@gmail.com>
2023-12-24 05:10:25 +0500

    Image SVG to canvas

---
+++ lib/src/paint_contents/image.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../paint_contents.dart';
+
+class ImageContent extends PaintContent {
+  ImageContent(this.info);
+
+  final PictureInfo info;
+
+  @override
+  PaintContent copy() {
+    return ImageContent(info);
+  }
+
+  @override
+  void draw(Canvas canvas, Size size, bool deeper) {
+    canvas.drawPicture(info.picture);
+  }
+
+  @override
+  void drawing(Offset nowPoint) {}
+
+  @override
+  void startDraw(Offset startPoint) {}
+
+  @override
+  Map<String, dynamic> toContentJson() {
+    return {};
+  }
+}
